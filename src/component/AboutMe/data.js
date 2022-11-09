d248b609738be6b83f833aa6949c49b013149050
DivyanshSareen <sareendivyansh@gmail.com>
2022-11-09 14:51:44 +0530

    refactor: edited about me text

--- src/component/AboutMe/data.js
+++ src/component/AboutMe/data.js
@@ -9,14 +9,14 @@ const data = [
     id: 1,
     text: [
       "A software developer from Delhi, India. I completed my Btech in Computer Science in June of this year and I am open for any web development jobs.",
-      "I have training my self in technologies like JavaScript, React, Redux, Webpack for the last ten months and currently I am exploring backend tech like Node and Express along with OpenAPI.",
+      "I have been training my self in technologies like JavaScript, React, Redux, Webpack for the last ten months and currently I am exploring backend tech like Node and Express along with OpenAPI.",
     ],
   },
   {
     id: 2,
     text: [
       "A software developer from Delhi, India. I completed my Btech in Computer Science in June of this year and I am open for any web development jobs.",
-      " I have training my self in technologies like JavaScript, React, Redux, Webpack for the last ten months and currently I am exploring backend tech like Node and Express along with OpenAPI.",
+      " I have been training my self in technologies like JavaScript, React, Redux, Webpack for the last ten months and currently I am exploring backend tech like Node and Express along with OpenAPI.",
       "Apart from my work, I am a simple guy who likes growing plants, taking longs walks and sometimes playing pokemon go :)",
     ],
   },
